da5e77e800b9fa0d36cfdbc05f4604dffff954b0
MDM <mdm.pro4@gmail.com>
2026-04-15 20:57:24 -0300

    feat: add sidebar and initial styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
 import { Header } from "./components/Header";
 import { Post } from "./Post";
+import styles from './App.module.css';
 import './global.css';
+import { Sidebar } from "./components/Sidebar";
 export function App() {
 
   return (
     <>
       <Header />
-      <Post
-        author="MDM Pro"
-        content="Desenvolvedor Full Stack"
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="MDM Pro"
+            content="Desenvolvedor Full Stack"
+          />
+          <Post
+            author="Ellen Barbosa"
+            content="Farmaceutica"
+          />
+        </main>
+      </div>
     </>)
 }
 
