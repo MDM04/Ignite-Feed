af8efe07ee4dde01edf7a4d65c5f95b9b2c88139
MDM <mdm.pro4@gmail.com>
2026-06-03 22:55:46 -0300

    feat: add Post component

---
+++ src/components/Post.jsx
@@ -0,0 +1,18 @@
+import styles from './Post.module.css';
+
+export function Post() {
+  return (
+    <article className={styles.post} >
+      <header>
+        <div className={styles.author}>
+          <img className={styles.avatar} src="https://github.com/diego3g.png" />
+          <div className={styles.authorInfo}>
+            <strong>Diego Fernandes</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+        <time title="11 de Maio às 08:13h" dateTime="2024-05-11 08:13:00">Publicado há 1h</time>
+      </header>
+    </article >
+  )
+}
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,11 +1,12 @@
 import { PencilLine } from 'phosphor-react'
+import capa from "../assets/ignite-capa.png"
 
 import styles from './Sidebar.module.css'
 
 export function Sidebar() {
     return (
         <aside className={styles.sidebar}>
-            <img className={styles.cover} src="https://plus.unsplash.com/premium_photo-1685086785636-2a1a0e5b591f?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+            <img className={styles.cover} src= {capa} />
             <div className={styles.profile}>
 
                 <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/116397937?v=4" alt="" />
