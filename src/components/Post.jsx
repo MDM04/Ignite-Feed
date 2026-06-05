c6043612a8e54d3c784af68222e76c91d52ea634
MDM <mdm.pro4@gmail.com>
2026-06-04 22:37:38 -0300

    feat: add styled  Post component

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -13,6 +13,16 @@ export function Post() {
         </div>
         <time title="11 de Maio às 08:13h" dateTime="2024-05-11 08:13:00">Publicado há 1h</time>
       </header>
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+        <p><a href="">jane.design/doctorcare</a></p>
+        <p>
+          <a href="">#novoprojeto</a>{' '}
+          <a href="">#nlw</a>{' '}
+          <a href="">#rocketseat</a>
+        </p>
+      </div>
     </article >
   )
 }
