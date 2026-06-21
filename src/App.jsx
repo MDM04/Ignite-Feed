ac3da6cfec1719dee0830c6793b9230211068377
MDM <mdm.pro4@gmail.com>
2026-06-21 19:57:32 -0300

    feat: add functionality to post component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,41 @@ import { Post } from "./components/Post";
 import styles from './App.module.css';
 import './global.css';
 import { Sidebar } from "./components/Sidebar";
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/diego3g.png',
+      name: 'Diego Fernandes',
+      role: 'CTO @Rocketseat'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galera 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'Diego.CTO/doctorcare' },
+    ],
+    publishedAt: new Date('2026-06-20 20:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/maykbrito.png',
+      name: 'Mayk Brito',
+      role: 'Educator @Rocketseat'
+    },
+    content: [
+      { type: 'paragraph', content: 'Boa noite👋' },
+      { type: 'paragraph', content: 'Fiz um novo projeto, confira lá no GitHub. 🚀' },
+      { type: 'link', content: 'mayk.educator/doctorcare' },
+    ],
+    publishedAt: new Date('2026-06-21 19:00:00'),
+  },
+];
+
+
+
+
 export function App() {
 
   return (
@@ -11,10 +46,16 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post
-            author="MDM Pro"
-            content="Desenvolvedor Full Stack"
-          />
+          {posts.map(post => {
+            return (
+              <Post
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
 
         </main>
       </div>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,29 +1,42 @@
 import { Avatar } from './Avatar';
 import { Comment } from './Comment';
 import styles from './Post.module.css';
+import { format, formatDistanceToNow } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 
-export function Post() {
+export function Post({ author, content, publishedAt }) {
+const publishedDateFormatted = format(publishedAt, "d 'de' MMMM 'às' HH:mm'h'", {
+  locale: ptBR
+});
+
+const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+  locale: ptBR,
+  addSuffix: true,
+})
+
   return (
     <article className={styles.post} >
       <header>
         <div className={styles.author}>
-          <Avatar src="https://github.com/diego3g.png" />
+          <Avatar src={author.avatarUrl} />
           <div className={styles.authorInfo}>
-            <strong>Diego Fernandes</strong>
-            <span>Web Developer</span>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
           </div>
         </div>
-        <time title="11 de Maio às 08:13h" dateTime="2024-05-11 08:13:00">Publicado há 1h</time>
+        <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>
+          {publishedDateRelativeToNow}
+        </time>
       </header>
       <div className={styles.content}>
-        <p>Fala galeraa 👋</p>
-        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-        <p><a href="">Diego.design/doctorcare</a></p>
-        <p>
-          <a href="">#novoprojeto</a>{' '}
-          <a href="">#nlw</a>{' '}
-          <a href="">#rocketseat</a>
-        </p>
+        {content.map(line => {
+          if (line.type === 'paragraph') {
+            return <p key={line.content}>{line.content}</p>
+          }
+          if (line.type === 'link') {
+            return <p key={line.content}><a href={line.content} target="_blank" rel="noopener noreferrer">{line.content}</a></p>
+          }
+        })}
       </div>
       <form className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
