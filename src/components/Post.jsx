e243fcb7f5aaeaf280f4f304a9751b2652f9f50b
MDM <mdm.pro4@gmail.com>
2026-07-02 22:23:56 -0300

    feat: implement comment deletion

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,39 +1,49 @@
+ import { format, formatDistanceToNow } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 import { useState } from 'react';
+
 import { Avatar } from './Avatar';
 import { Comment } from './Comment';
+
 import styles from './Post.module.css';
-import { format, formatDistanceToNow } from 'date-fns';
-import ptBR from 'date-fns/locale/pt-BR';
 
-export function Post({ author, content, publishedAt }) {
+export function Post({ author, publishedAt, content }) {
+  const [comments, setComments] = useState([
+    'Post muito bacana, hein?!'
+  ]);
 
-  const [comments, setComments] = useState(['Post muito bacana, hein?!']);
+  const [newCommentText, setNewCommentText] = useState('');
 
-  const [newComment, setNewComment] = useState('');
-
-  const publishedDateFormatted = format(publishedAt, "d 'de' MMMM 'às' HH:mm'h'", {
-    locale: ptBR
+  const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
+    locale: ptBR,
   });
 
   const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
     locale: ptBR,
-    addSuffix: true,
-  })
+    addSuffix: true
+  });
 
-  function handleNewCommentChange(event) {
-    setNewComment(event.target.value);
+  function handleCrateNewComment() {
+    event.preventDefault()
+
+    setComments([...comments, newCommentText]);
+    setNewCommentText('');
   }
 
-  function handleCreateNewComment(event) {
-    event.preventDefault();
+  function handleNewCommentChange() {
+    setNewCommentText(event.target.value);
+  }
+ 
+ function deleteComment(commentToDelete) {
+    const commentsWithoutDeletedOne = comments.filter(comment => {
+      return comment !== commentToDelete;
+    })
 
-
-    setComments([...comments, newComment]);
-    setNewComment('');
+    setComments(commentsWithoutDeletedOne);
   }
 
   return (
-    <article className={styles.post} >
+    <article className={styles.post}>
       <header>
         <div className={styles.author}>
           <Avatar src={author.avatarUrl} />
@@ -42,35 +52,48 @@ export function Post({ author, content, publishedAt }) {
             <span>{author.role}</span>
           </div>
         </div>
+
         <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>
           {publishedDateRelativeToNow}
         </time>
       </header>
+
       <div className={styles.content}>
         {content.map(line => {
           if (line.type === 'paragraph') {
-            return <p key={line.content}>{line.content}</p>
-          }
-          if (line.type === 'link') {
-            return <p key={line.content}><a href={line.content} target="_blank" rel="noopener noreferrer">{line.content}</a></p>
+            return <p key={line.content}>{line.content}</p>;
+          } else if (line.type === 'link') {
+            return <p key={line.content}><a href="#">{line.content}</a></p>
           }
         })}
       </div>
-      <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
+
+      <form onSubmit={handleCrateNewComment} className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
-        <textarea name="textarea"
-         placeholder="Deixe um comentário" 
-         value={newComment}
-         onChange = {handleNewCommentChange}/>
+
+        <textarea
+          name="comment"
+          placeholder="Deixe um comentário"
+          value={newCommentText}
+          onChange={handleNewCommentChange}
+        />
+
         <footer>
           <button type="submit">Publicar</button>
         </footer>
       </form>
+
       <div className={styles.commentList}>
         {comments.map(comment => {
-          return <Comment content={comment} />
+          return (
+            <Comment
+              key={comment}
+              content={comment}
+              onDeleteComment={deleteComment}
+            />
+          )
         })}
       </div>
-    </article >
+    </article>
   )
 }
